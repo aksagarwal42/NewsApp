c4b38435b42f559214de740eabd1904d73461041
aksagarwal42 <aksagarwal42@gmail.com>
2021-08-31 15:07:30 +0530

    icon removed and api key changed

--- lib/news_data.dart
+++ lib/news_data.dart
@@ -15,7 +15,7 @@ class NewsData {
 Future<NewsData> getNewsArticles(String filter) async {
   String urlDomain = 'https://newsapi.org';
   String urlVersion = '/v2';
-  String apiKey = '49afcb3e05554f62842b1dfd630ce594';
+  String apiKey = '4cafb8efcd9c4487be6582dae6a1a8e2';
 
   String url = urlDomain + urlVersion + filter + '&country=in&apiKey=' + apiKey;
 
